c296675abd1b8b70ffe3ea1371f9359b94ed061d
mufasa1976 <mufasa1976@coolstuff.software>
2019-01-20 04:30:31 +0100

    some polishing

--- src/main/java/io/github/mufasa1976/meetup/springboottest/config/SecurityConfiguration.java
+++ src/main/java/io/github/mufasa1976/meetup/springboottest/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package io.github.mufasa1976.meetup.springboottest.config;
 
+import io.github.mufasa1976.meetup.springboottest.Routes;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,7 +12,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-        .antMatchers("/api/**").authenticated()
+        .antMatchers(Routes.API + "/**").authenticated()
         .anyRequest().permitAll()
         .and()
         .httpBasic();
