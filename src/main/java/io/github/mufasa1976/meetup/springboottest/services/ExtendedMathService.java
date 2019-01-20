13e6c089833b0c9b4e5ad9010b3790382da9b428
mufasa1976 <mufasa1976@coolstuff.software>
2019-01-20 16:23:56 +0100

    added Test for MathService with SpringBootTest

--- src/main/java/io/github/mufasa1976/meetup/springboottest/services/ExtendedMathService.java
+++ src/main/java/io/github/mufasa1976/meetup/springboottest/services/ExtendedMathService.java
@@ -1,5 +1,5 @@
 package io.github.mufasa1976.meetup.springboottest.services;
 
 public interface ExtendedMathService {
-  int divide(int divisor, int divident);
+  int divide(int dividend, int divisor);
 }
--- src/main/java/io/github/mufasa1976/meetup/springboottest/services/ExtendedMathServiceImpl.java
+++ src/main/java/io/github/mufasa1976/meetup/springboottest/services/ExtendedMathServiceImpl.java
@@ -9,9 +9,9 @@ public class ExtendedMathServiceImpl implements ExtendedMathService {
   private final MathService mathService;
 
   @Override
-  public int divide(int divisor, int divident) {
+  public int divide(int dividend, int divisor) {
     try {
-      return mathService.divide(divisor, divident);
+      return mathService.divide(dividend, divisor);
     } catch (ArithmeticException e) {
       throw new IllegalArgumentException(e.getMessage(), e);
     }
