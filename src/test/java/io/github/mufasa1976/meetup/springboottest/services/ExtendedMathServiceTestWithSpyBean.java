92c561c809a0241c3968d4d832ab8bcb8745356a
mufasa1976 <mufasa1976@coolstuff.software>
2019-01-21 07:43:04 +0100

    added ExtendedMatchServiceTestWithSpyBean.java

---
+++ src/test/java/io/github/mufasa1976/meetup/springboottest/services/ExtendedMathServiceTestWithSpyBean.java
@@ -0,0 +1,43 @@
+package io.github.mufasa1976.meetup.springboottest.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = { MathServiceImpl.class, ExtendedMathServiceImpl.class })
+public class ExtendedMathServiceTestWithSpyBean {
+  @SpyBean
+  private MathService mathService;
+
+  @Autowired
+  private ExtendedMathService extendedMathService;
+
+  @Test
+  public void divide_OK() {
+    assertThat(extendedMathService.divide(4, 2)).isEqualTo(2);
+    verify(mathService, times(1)).divide(anyInt(), anyInt());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void divide_NOK() {
+    try {
+      extendedMathService.divide(1, 0);
+    } finally {
+      verify(mathService).divide(1, 0);
+    }
+  }
+
+  @Test
+  public void divide_OK_mockedDivideByZero() {
+    doReturn(Integer.MAX_VALUE).when(mathService).divide(1, 0);
+    assertThat(extendedMathService.divide(1, 0)).isEqualTo(Integer.MAX_VALUE);
+  }
+}
